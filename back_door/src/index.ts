2d70a8b4b21e4dc3ea866eb9da491ff6b840b450
Neirotciv <tanacetifolia@gmail.com>
2023-08-28 14:37:37 +0200

    enable all CORS requests

--- back_door/src/index.ts
+++ back_door/src/index.ts
@@ -4,9 +4,11 @@ const index = require('./routes/index');
 const bodyParser = require('body-parser');
 const app: Express = express();
 const port: number = 8000;
+const cors = require('cors');
 
 app.use(express.json());
 app.use('/', index);
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
